ea7f38bf51572dfc8e14490a145ce4d00d2cac0c
kuldeep230 <ramdeep214@gmail.com>
2021-12-02 22:48:33 +0530

    github pages test

--- src/App.js
+++ src/App.js
@@ -14,20 +14,24 @@ const App = () => {
     <div className="bg-gradient-to-r from-purple-500 to-red-500 px-4 sm:px-6 lg:px-16    hover:from-pink-700 hover:to-yellow-400 ">
       <GithubState>
         <div>
-          <Navbar title="GitHubInfo" icon="fab fa-github" />
-        </div>
-        <div>
           <Switch>
-            <Route exact path="/about" component={About} />
-            <Route exact path="/contact" component={Contact} />{" "}
+            <Route exact path="/">
+              <Navbar title="GitHubInfo" icon="fab fa-github" />
+              <Users />
+            </Route>
+            <Route exact path="/about">
+              <Navbar title="GitHubInfo" icon="fab fa-github" />
+              <About />
+            </Route>
+            <Route exact path="/contact">
+              <Navbar title="GitHubInfo" icon="fab fa-github" />
+              <Contact />
+            </Route>
             <Route
               exact
               path="/user/:userId"
               render={(props) => <Profile {...props} />}
             />
-            <Route exact path="/">
-              <Users />
-            </Route>
           </Switch>
         </div>
       </GithubState>
